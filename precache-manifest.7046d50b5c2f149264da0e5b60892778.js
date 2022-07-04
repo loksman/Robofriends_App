f6a745cd591a1a4dfc009a0f34eb137f0495dcc0
Lokkur Zingfa <zlokkur@gmail.com>
2022-07-04 21:53:39 +0100

    Updates

---
+++ precache-manifest.7046d50b5c2f149264da0e5b60892778.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab254767c8ef775da605",
+    "url": "/Robofriends_App/static/css/main.d6af76bb.chunk.css"
+  },
+  {
+    "revision": "ab254767c8ef775da605",
+    "url": "/Robofriends_App/static/js/main.ab254767.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Robofriends_App/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "f05915520587e6bdfeb4",
+    "url": "/Robofriends_App/static/css/2.5beeae23.chunk.css"
+  },
+  {
+    "revision": "f05915520587e6bdfeb4",
+    "url": "/Robofriends_App/static/js/2.f0591552.chunk.js"
+  },
+  {
+    "revision": "a5c8cb9f8ab373df3422",
+    "url": "/Robofriends_App/static/js/runtime~main.a5c8cb9f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends_App/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "77bf7ef9049aa51df5addb1cf28095ff",
+    "url": "/Robofriends_App/index.html"
+  }
+];
